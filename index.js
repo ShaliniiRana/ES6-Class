fd91030727e6259edc813ce539356498b6fcc537
ShaliniiRana <122456607+ShaliniiRana@users.noreply.github.com>
2023-07-21 12:48:50 +0530

    index.js

--- index.js
+++ index.js
@@ -1,23 +1,31 @@
+//Write your code here
 class API {
-    constructor(url, method = 'GET') {
-      this.url = url;
-      this.method = method;
-      this.#updateSecure();
-    }
-  
-    #secure = false;
-  
-    #updateSecure() {
-      this.#secure = this.url.startsWith('https');
-    }
-  
-    isSecure() {
-      return this.#secure;
-    }
-  
-    updateURL(newURL) {
-      this.url = newURL;
-      this.#updateSecure();
-    }
+  url;
+  method;
+
+  #secure;
+
+  constructor(url, method = 'GET') {
+    this.url = url;
+    this.method = method;
+    this.#secure = url.startsWith('https');
   }
-  
+
+  isSecure() {
+    return this.#secure;
+  }
+
+  updateUrl(url) {
+    this.url = url;
+    this.#secure = url.startsWith('https');
+  }
+}
+
+
+//testing
+
+
+
+
+//Do not remove this code
+module.exports = { API }
